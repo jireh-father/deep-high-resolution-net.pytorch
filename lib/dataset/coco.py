7efce397c12351f14ef03a4fd260b0a1abc2464c
seoilgun <seoilgun@gmail.com>
2020-09-23 02:41:10 +0900

    modify dataset

--- lib/dataset/coco.py
+++ lib/dataset/coco.py
@@ -435,6 +435,7 @@ class COCODataset(JointsDataset):
 
     def _do_python_keypoint_eval(self, res_file, res_folder):
         coco_dt = self.coco.loadRes(res_file)
+        print(coco_dt)
         coco_eval = COCOeval(self.coco, coco_dt, 'keypoints')
         coco_eval.params.useSegm = None
         coco_eval.evaluate()
